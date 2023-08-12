289df1e7f94c0eccf18f88b8ebd3d06380ed0632
roland-a <91647122+roland-a@users.noreply.github.com>
2023-08-12 07:06:02 -0400

    villager golems will also target those actively targeting villagers

--- src/main/java/unaverage/tweaks/mixin/ActiveTargetMixin.java
+++ src/main/java/unaverage/tweaks/mixin/ActiveTargetMixin.java
@@ -1,10 +1,15 @@
 package unaverage.tweaks.mixin;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 import net.minecraft.entity.ai.goal.TrackTargetGoal;
-import net.minecraft.entity.mob.*;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.raid.RaiderEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,7 +33,7 @@ public abstract class ActiveTargetMixin extends TrackTargetGoal {
         if (GlobalConfig.Miscellaneous.village_golems_only_fight_threats) return;
 
         if (mob instanceof ZombieEntity) return;
-        if (mob instanceof PatrolEntity) return;
+        if (mob instanceof RaiderEntity) return;
 
         if (targetClass != IronGolemEntity.class) return;
 
@@ -41,7 +46,7 @@ public abstract class ActiveTargetMixin extends TrackTargetGoal {
         method = "<init>(Lnet/minecraft/entity/mob/MobEntity;Ljava/lang/Class;IZZLjava/util/function/Predicate;)V",
         at = @At("TAIL")
     )
-    public void injectVillagerGolemsDontAttackMobs(MobEntity mob, Class<?> targetClass, int reciprocalChance, boolean checkVisibility, boolean checkCanNavigate, Predicate<?> targetPredicate, CallbackInfo ci){
+    public void injectVillagerGolemsDontAttackMobs(MobEntity mob, Class<?> targetClass, int reciprocalChance, boolean checkVisibility, boolean checkCanNavigate, Predicate<LivingEntity> targetPredicate, CallbackInfo ci){
         if (!GlobalConfig.Miscellaneous.village_golems_only_fight_threats) return;
 
         if (!(mob instanceof IronGolemEntity)) return;
@@ -50,14 +55,19 @@ public abstract class ActiveTargetMixin extends TrackTargetGoal {
 
         this.targetPredicate = this.targetPredicate.setPredicate(
             e -> {
-                if (!((IronGolemEntity)mob).isPlayerCreated()){
-                    return e instanceof ZombieEntity || e instanceof PatrolEntity;
+                if (((IronGolemEntity)mob).isPlayerCreated()){
+                    return targetPredicate.test(e);
                 }
 
                 if (!(e instanceof Monster)) return false;
-                if (e instanceof CreeperEntity) return false;
+                if (e instanceof ZombieEntity) return true;
+                if (e instanceof RaiderEntity) return true;
 
-                return true;
+                var targetOfTarget = ((MobEntity)e).getTarget();
+                if (targetOfTarget instanceof IronGolemEntity) return true;
+                if (targetOfTarget instanceof VillagerEntity) return true;
+
+                return false;
             }
         );
     }
