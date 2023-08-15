4209fa81c806247996f0904a2ad7677917f5d572
roland-a <91647122+roland-a@users.noreply.github.com>
2023-08-15 15:55:48 -0400

    Added "disable xp" feature

---
+++ src/main/java/unaverage/tweaks/mixin/XpOrbMixin.java
@@ -0,0 +1,32 @@
+package unaverage.tweaks.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import unaverage.tweaks.GlobalConfig;
+
+@Mixin(ExperienceOrbEntity.class)
+abstract class XpOrbMixin extends Entity {
+    public XpOrbMixin(EntityType<?> type, World world) {super(type, world);}
+    @Shadow private int amount;
+
+    @Inject(
+        method = "tick",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    void fn(CallbackInfo ci){
+        if (!GlobalConfig.XP.disable_xp) return;
+
+        this.amount = 0;
+        this.discard();
+
+        ci.cancel();
+    }
+}
