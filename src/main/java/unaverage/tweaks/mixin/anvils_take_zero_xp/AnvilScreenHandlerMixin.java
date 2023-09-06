49d481cbbb5c0c5707053903e4012f11baccd4e8
roland-a <91647122+roland-a@users.noreply.github.com>
2023-09-06 00:33:46 -0400

    Fixed the "anvil take zero xp" feature still taking xp

--- src/main/java/unaverage/tweaks/mixin/anvils_take_zero_xp/AnvilScreenHandlerMixin.java
+++ src/main/java/unaverage/tweaks/mixin/anvils_take_zero_xp/AnvilScreenHandlerMixin.java
@@ -2,18 +2,21 @@ package unaverage.tweaks.mixin.anvils_take_zero_xp;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.screen.AnvilScreenHandler;
-import net.minecraft.screen.ForgingScreenHandler;
-import net.minecraft.screen.ScreenHandlerContext;
-import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.*;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
+    @Shadow @Final private Property levelCost;
+
     public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {super(type, syncId, playerInventory, context);}
 
     @Inject(
@@ -23,5 +26,13 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
     )
     void noLongerNeedsXP(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> cir){
         cir.setReturnValue(true);
+    }
+
+    @Inject(
+        method = "onTakeOutput",
+        at = @At("HEAD")
+    )
+    void noLongerTakesXP(PlayerEntity player, ItemStack stack, CallbackInfo ci){
+        this.levelCost.set(0);
     }
 }
