0de839d78a2d2d0a5de9dd417bfc7e32b0a19597
roland-a <91647122+roland-a@users.noreply.github.com>
2024-03-02 23:13:29 -0500

    Added message for trying to sleep outside the overworld

--- src/main/java/unaverage/tweaks/mixin/beds_dont_explode/BedMixin.java
+++ src/main/java/unaverage/tweaks/mixin/beds_dont_explode/BedMixin.java
@@ -1,8 +1,14 @@
 package unaverage.tweaks.mixin.beds_dont_explode;
 
 import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -10,7 +16,9 @@ import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 //TODO add message
 @Mixin(BedBlock.class)
@@ -29,5 +37,16 @@ public class BedMixin {
     )
     Explosion disableExplosion(World instance, Entity entity, DamageSource damageSource, ExplosionBehavior behavior, Vec3d pos, float power, boolean createFire, World.ExplosionSourceType explosionSourceType){
         return null;
+    }
+
+    @Inject(
+        method = "onUse",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;Lnet/minecraft/util/math/Vec3d;FZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"
+        )
+    )
+    void tellPlayer(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir){
+        player.sendMessage(Text.translatable("unaverage_tweaks.bed_message"), true);
     }
 }
